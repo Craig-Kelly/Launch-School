a21fed1a2fa2ea5e18adf663795fe438d6aebb5a
Craig Kelly <craig.kelly@shopify.com>
2019-08-22 14:50:43 -0400

    add exercise

---
+++ RB101/easy3/square.rb
@@ -0,0 +1,20 @@
+=begin
+
+Squaring an Argument
+Using the multiply method from the "Multiplying Two Numbers" problem,
+write a method that computes the square of its argument (the square is the result of multiplying a number by itself).
+
+Example:
+
+square(5) == 25
+square(-8) == 64
+=end
+
+def square(num)
+  p num * num
+
+end
+
+square(-5)
+square(8)
+square(5.5)
