74f718e0cbceb31493012ec5b52d06b83f1783e5
Craig Kelly <craig.kelly@shopify.com>
2019-09-10 16:48:57 -0400

    add exercise

---
+++ RB101/easy3/odd_list.rb
@@ -0,0 +1,29 @@
+=begin
+
+Odd Lists
+Write a method that returns an Array that contains every other element of an Array that is passed in as an argument. The values in the returned list should be those values that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
+
+Examples:
+
+oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+oddities(['abc', 'def']) == ['abc']
+oddities([123]) == [123]
+oddities([]) == []
+
+=end
+
+require 'pry'
+
+def oddities(arr2)
+  arr = []
+  arr2.each.with_index{|x, i| arr << x if i % 2 == 0}
+  return arr
+end
+
+p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+p oddities(['abc', 'def']) == ['abc']
+p oddities([123]) == [123]
+p oddities([]) == []
+
