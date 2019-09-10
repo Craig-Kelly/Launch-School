1f0a79843de9f4f6dbc8811cc0e2c3cb161133d6
Craig Kelly <craig.kelly@shopify.com>
2019-09-10 18:51:27 -0400

    dir easy5 add ascii_value

---
+++ RB101/easy5/ascii_value.rb
@@ -0,0 +1,27 @@
+=begin
+
+Write a method that determines and returns the ASCII string value of a string that is passed in as an argument. The ASCII string value is the sum of the ASCII values of every character in the string. (You may use String#ord to determine the ASCII value of a character.)
+
+ascii_value('Four score') == 984
+ascii_value('Launch School') == 1251
+ascii_value('a') == 97
+ascii_value('') == 0
+
+=end
+
+def ascii_value(str)
+  if str == ''
+    0
+  else 
+    str
+    .split('')
+    .map { |x| x.ord }
+    .inject(:+)
+  end
+end
+
+p ascii_value('Four score') == 984
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0
+
