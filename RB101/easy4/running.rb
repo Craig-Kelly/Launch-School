f9446d2bf3196025a6c8eb6301e58257f75b5968
Craig Kelly <craig.kelly@shopify.com>
2019-09-12 15:03:28 -0400

    add exercise

--- RB101/easy4/running.rb
+++ RB101/easy4/running.rb
@@ -12,6 +12,19 @@ running_total([]) == []
 
 =end
 
+require 'pry'
+
 def running_total(ary)
 
+  ary2 = [0]
+    until ary.length == 0
+      ary2 << ary.shift + ary2[-1]
+    end
+  ary2.shift
+  return ary2
 end
+
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []
